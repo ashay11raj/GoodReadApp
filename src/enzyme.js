3101cc8b0fd35f51899256ae662edd36992f149c
unknown <ashay11raj@gmail.com>
2019-04-21 19:41:40 +0530

    Updates

--- src/enzyme.js
+++
@@ -1,6 +0,0 @@
-import Enzyme, { configure, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-Enzyme.configure({ adapter: new Adapter() });
-export { shallow };
-export default Enzyme;
